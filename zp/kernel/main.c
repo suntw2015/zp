8b3bb995c7f00dc48287209c7eb71a30dad0fb89
suntianwen002 <suntianwen002@ke.com>
2022-01-07 17:42:11 +0800

    fix

--- zp/kernel/main.c
+++ zp/kernel/main.c
@@ -13,6 +13,18 @@
 void Start_Kernel(void)
 {
 	char * addr = (char *)0xffff800000a00000;
+
+	int i,j,offset;
+	for (i=50;i<70;i++) {
+		for (j=0;j<1400;j++) {
+			offset = (i*1400+j) * 4;
+			*(addr+offset+0) = (char)0x00;
+			*(addr+offset+1) = (char)0x00;
+			*(addr+offset+2) = (char)0xff;
+			*(addr+offset+3) = (char)0x00;
+		}
+	}
+
 	displayChar(addr, 0, 0, 'A', (char)0xFF, (char)0x00, (char)0x00, (char)0x00);
 
 	char str[] = "hello";
