4827945d00b2074668cfe5173a1caba4a6d7fe24
suntianwen002 <suntianwen002@ke.com>
2022-01-07 19:09:58 +0800

    fix

--- zp/kernel/main.c
+++ zp/kernel/main.c
@@ -2,13 +2,13 @@
 #include "font.h"
 
 //全局变量
-// KernelConfig kernelConfig;
+KernelConfig kernelConfig;
 
-// void init() {
-// 	kernelConfig.screenMaxCol = 1400;
-// 	kernelConfig.screenMaxRow = 960;
-// 	kernelConfig.screenAddress = (char *)0xffff800000a00000;
-// }
+void init() {
+	kernelConfig.screenMaxCol = 1400;
+	kernelConfig.screenMaxRow = 960;
+	kernelConfig.screenAddress = (char *)0xffff800000a00000;
+}
 
 void Start_Kernel(void)
 {
