35dd214ffa207e34793c019abf92ce47439c4d0a
suntianwen002 <suntianwen002@ke.com>
2022-01-07 16:44:43 +0800

    fix

--- zp/kernel/main.c
+++ zp/kernel/main.c
@@ -4,15 +4,17 @@
 //全局变量
 KernelConfig kernelConfig;
 
-void Start_Kernel(void)
-{
-	init();
-	displayChar(kernelConfig.screenAddress, 0, 0, 'A', 0xFF, 0, 0, 0);
-	displayString(kernelConfig.screenAddress, 20, 0, 'Hello', 0xFF, 0, 0, 0);
-}
-
 void init() {
 	kernelConfig.screenMaxCol = 1400;
 	kernelConfig.screenMaxRow = 960;
 	kernelConfig.screenAddress = (char *)0xffff800000a00000;
+}
+
+void Start_Kernel(void)
+{
+	init();
+	displayChar(kernelConfig.screenAddress, 0, 0, 'A', 0xFF, 0, 0, 0);
+
+	char str[] = "hello";
+	displayString(kernelConfig.screenAddress, 20, 0, str, 0xFF, 0, 0, 0);
 }
