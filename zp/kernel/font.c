dcc29e422f14a1d9859504d3ce684798f75e8530
suntianwen002 <suntianwen002@ke.com>
2022-01-07 17:27:30 +0800

    fix

--- zp/kernel/font.c
+++ zp/kernel/font.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 int getPointAddressOffset(int x, int y) {
-    return (x * kernelConfig.screenMaxCol + y) * 4;
+    return (x * 1400 + y) * 4;
 }
 
 //显示一个点
--- zp/kernel/main.c
+++ zp/kernel/main.c
@@ -2,19 +2,19 @@
 #include "font.h"
 
 //全局变量
-KernelConfig kernelConfig;
+// KernelConfig kernelConfig;
 
-void init() {
-	kernelConfig.screenMaxCol = 1400;
-	kernelConfig.screenMaxRow = 960;
-	kernelConfig.screenAddress = (char *)0xffff800000a00000;
-}
+// void init() {
+// 	kernelConfig.screenMaxCol = 1400;
+// 	kernelConfig.screenMaxRow = 960;
+// 	kernelConfig.screenAddress = (char *)0xffff800000a00000;
+// }
 
 void Start_Kernel(void)
 {
 	init();
-	displayChar(kernelConfig.screenAddress, 0, 0, 'A', 0xFF, 0, 0, 0);
+	displayChar((char *)0xffff800000a00000, 0, 0, 'A', 0xFF, 0, 0, 0);
 
 	char str[] = "hello";
-	displayString(kernelConfig.screenAddress, 20, 0, str, 0xFF, 0, 0, 0);
+	displayString((char *)0xffff800000a00000, 20, 0, str, 0xFF, 0, 0, 0);
 }
